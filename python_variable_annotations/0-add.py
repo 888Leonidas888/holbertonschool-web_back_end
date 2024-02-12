146dc1e7f7039d74dd2d5daf836756e701059c01
Jhony <Jhonny14_1@hotmail.com>
2024-02-12 10:49:48 -0500

    feat: se agrega función que suma 2 números

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Anotaciones en funciones con Python"""
+
+def add(a: float, b: float) -> float:
+    """Suma dos números de punto flotante.
+
+    Args:
+        a (float): El primer número.
+        b (float): El segundo número.
+
+    Returns:
+        float : La suma de a y b.
+    """
+    return a + b
