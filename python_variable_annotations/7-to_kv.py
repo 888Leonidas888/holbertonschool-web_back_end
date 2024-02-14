0e987af8c4c72bbbe1c45dc279e3a141a4d93912
Jhony <Jhonny14_1@hotmail.com>
2024-02-14 10:52:33 -0500

    feat: se sube función con anotaciones complejas y tests

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/dev python3
+"""Anotaciones en Python de tipos complejos."""
+
+
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int | float]) -> Union[str, float]:
+    """Eleva al cuadrado el segundo argumento.
+
+    Args:
+        k (str): Primer argumento.
+        v (int | float): segundo argumento.
+
+    Returns:
+        tupla : Regresa (k , v ** 2)
+    """
+    return (k, v ** 2)
