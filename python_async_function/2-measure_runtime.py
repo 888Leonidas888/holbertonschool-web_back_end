a1006413d7448582f84b3f3b6d6b28ae4cca99ac
Jhony <Jhonny14_1@hotmail.com>
2024-02-21 15:56:39 -0500

    feat: Mide el tiempo promedio de las tareas.

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Python asíncrono: medir el tiempo de ejecución."""
+
+from time import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Mide el tiempo promedio de ejecución de cada tarea.
+
+        Args:
+            n (int): Las veces que debe ejecutar la tarea.
+            max_delay (int): Límite superior de random.
+
+        Returns:
+            float : Tiempo promedio tiempo_total / n
+    """
+    start = time()
+    wait_n(n, max_delay)
+    finish = time()
+    return (finish - start) / n
