f45a2daa694ef8eee9083e0f2457b0dc8c4dfe2f
Jhony <Jhonny14_1@hotmail.com>
2024-02-21 16:01:23 -0500

    docs: se actualiza

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 """Python asíncrono."""
 
-import asyncio
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Lamzamos varias tareas en paralelo."""
+    """Lamzamos varias tareas en paralelo.
+
+        Args:
+            n (int): Las veces que debe ejecutar la tarea.
+            max_delay (int): Límite superior para random.
+
+        Returns:
+            List[float]: Una lista con tiempo de espera.
+    """
     temp_list = []
-    for i in range(n):
+    for _ in range(n):
         temp_list.append(await wait_random(max_delay))
     return sorted(temp_list)
