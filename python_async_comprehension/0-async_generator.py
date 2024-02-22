a2b464aafbfb9a3147b380707021c10f05511432
Jhony <Jhonny14_1@hotmail.com>
2024-02-22 16:56:24 -0500

    feat: Generadores asíncronos task#1.

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Comprensión asíncrona y generadores asíncronos."""
+
+import asyncio
+from typing import AsyncGenerator
+from random import uniform
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Coroutina que genera valores aleatorios entre 0 y 10
+    de manera asíncrona.
+
+    Cada iteración espera 1 segundo para producir el siguiente número.
+
+    Yields:
+        float: Float entre 0 y 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
