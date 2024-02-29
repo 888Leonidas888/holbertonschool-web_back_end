9e33f44007bfbcb2d42ae2408e3b40bf2c1f40d6
Jhony <Jhonny14_1@hotmail.com>
2024-02-29 13:35:14 -0500

    refactor: Se usa comprensión de listas y bucles para reducir la cantidad de print().

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,20 +6,23 @@ from pymongo import MongoClient
 
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    collection = client['logs']['nginx']
+
     total_documents = collection.count_documents({})
-    total_get = collection.count_documents({"method": "GET"})
-    total_post = collection.count_documents({"method": "POST"})
-    total_put = collection.count_documents({"method": "PUT"})
-    total_patch = collection.count_documents({"method": "PATCH"})
-    total_delete = collection.count_documents({"method": "DELETE"})
     total_status = collection.count_documents({"path": "/status"})
+
+    methods = [
+        {"method": "GET"},
+        {"method": "POST"},
+        {"method": "PUT"},
+        {"method": "PATCH"},
+        {"method": "DELETE"},
+    ]
+
+    totales = [collection.count_documents(method) for method in methods]
 
     print(f'{total_documents} logs')
     print('Methods:')
-    print(f'    method GET: {total_get}')
-    print(f'    method POST: {total_post}')
-    print(f'    method PUT: {total_put}')
-    print(f'    method PATCH: {total_patch}')
-    print(f'    method DELETE: {total_delete}')
+    for i, t in enumerate(totales):
+        print(f'\tmethod {methods[i].get("method")}: {t}')
     print(f'{total_status} status check')
