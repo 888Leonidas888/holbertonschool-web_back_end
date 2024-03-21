c450c7cf6f98d28572642b43f338004d214c2c8b
Jhony <Jhonny14_1@hotmail.com>
2024-03-21 18:42:55 -0500

    feat: Esta clase implementa un método de instancia y un método estático.

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+// Class Pricing
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
