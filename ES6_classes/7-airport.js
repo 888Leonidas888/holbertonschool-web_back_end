ddcff03c7535ee0c0c3302334be0d88b53406e34
Jhony <Jhonny14_1@hotmail.com>
2024-03-22 15:43:27 -0500

    feat(7-airport): Esta clase implementa el metodo toString().

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+// Class Airport
+
+export default class Airport {
+  constructor(name = '', code = '') {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
