06b50e58873b20a3a8a324d7534aa1e2d3700f9a
Jhony <Jhonny14_1@hotmail.com>
2024-03-28 17:56:45 -0500

    feat(8-getBudgetCurrencyYear): Modifica función para utilizar propiedades calculadas.

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+// Property calculate
+
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
