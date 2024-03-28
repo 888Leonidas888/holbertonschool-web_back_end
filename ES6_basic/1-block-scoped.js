0cadfe8c44f1d7c19af1faa09f3e58c109b127ab
Jhony <Jhonny14_1@hotmail.com>
2024-03-28 16:13:04 -0500

    feat(1-block-scoped): Se modifica código base, uso de let.

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+// Block scope
+
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
