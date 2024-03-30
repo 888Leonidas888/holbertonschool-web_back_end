a1960f5cd0b0afcef90a4bd3f3c2dae090ba4983
Jhony <Jhonny14_1@hotmail.com>
2024-03-30 12:27:47 -0500

    feat(11-createEmployeesObject): Crea un mapa.

---
+++ ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,9 @@
+// Iterator
+
+export default function createEmployeesObject(departmentName, employees) {
+  const employeesObject = {};
+
+  employeesObject[departmentName] = employees;
+
+  return employeesObject;
+}
