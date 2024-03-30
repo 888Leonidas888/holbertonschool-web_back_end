8285a4375bf0268b22951dddf0c5701ed13c7119
Jhony <Jhonny14_1@hotmail.com>
2024-03-30 11:29:06 -0500

    feat(9-getBudgetObject): Se reescribe script para aplicar sintaxis de propiedades de métodos en ES6.

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+// ES6 sintasix de propiedades de métodos
+
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
