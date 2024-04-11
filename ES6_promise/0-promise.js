7feb60f82d34233139519682c9e5bb6d24f9b9d6
Jhony <Jhonny14_1@hotmail.com>
2024-04-10 20:35:14 -0500

    chore(0-promise): Se modifica estilos.

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,5 @@
 // Keep every promise you make and only make promises you can keep
 
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve;
-  });
+  return new Promise((resolve) => resolve);
 }
