09dc90061427bc031861bbe265bc2c092e27788e
Jhony <Jhonny14_1@hotmail.com>
2024-04-10 20:45:16 -0500

    chore(2-then): Regresa a la version anterior.

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,32 +1,16 @@
 // Catch me if you can!
 
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-        const result = {
-            status: 200,
-            body: 'success',
-        };
-        if (promise) {
-            console.log('Got a response from the API');
-            resolve(result);
-        } else {
-            console.log('Got a response from the API');
-      reject(new Error());
-    }
-});
+  return promise
+    .then(() => {
+      const response = {
+        status: 200,
+        body: 'success',
+      };
+      return response;
+    })
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
-
-// export default function handleResponseFromAPI(promise) {
-//   return promise
-//     .then(() => {
-//       const response = {
-//         status: 200,
-//         body: 'success',
-//       };
-//       return response;
-//     })
-//     .catch(() => new Error())
-//     .finally(() => {
-//       console.log('Got a response from the API');
-//     });
-// }
