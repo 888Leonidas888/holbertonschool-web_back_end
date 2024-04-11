f80e5bf19f39b1a0364b41c1116fdb7b56842833
Jhony <Jhonny14_1@hotmail.com>
2024-04-10 20:43:01 -0500

    refactor(2-then): Se cambia estructura.

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,32 +1,32 @@
 // Catch me if you can!
 
-// export default function handleResponseFromAPI(promise) {
-//     return new Promise((resolve, reject) => {
-//         const result = {
-//             status: 200,
-//             body: 'success',
-//         };
-//         if (promise) {
-//             console.log('Got a response from the API');
-//             resolve(result);
-//         } else {
-//             console.log('Got a response from the API');
-//       reject(new Error());
-//     }
-// });
-// }
-
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
-      const response = {
-        status: 200,
-        body: 'success',
-      };
-      return response;
-    })
-    .catch(() => new Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    return new Promise((resolve, reject) => {
+        const result = {
+            status: 200,
+            body: 'success',
+        };
+        if (promise) {
+            console.log('Got a response from the API');
+            resolve(result);
+        } else {
+            console.log('Got a response from the API');
+      reject(new Error());
+    }
+});
 }
+
+// export default function handleResponseFromAPI(promise) {
+//   return promise
+//     .then(() => {
+//       const response = {
+//         status: 200,
+//         body: 'success',
+//       };
+//       return response;
+//     })
+//     .catch(() => new Error())
+//     .finally(() => {
+//       console.log('Got a response from the API');
+//     });
+// }
