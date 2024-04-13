f95b385d042293d34f27bc5f0f908f17bbc93e97
Jhony <Jhonny14_1@hotmail.com>
2024-04-13 10:08:18 -0500

    chore: Función que maneja varias promesas.

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,1 +1,17 @@
-console.log('Hi')
+// Handle multiple successful promises
+
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([responsePhoto, responseUser]) => {
+      console.log(
+        responsePhoto.body,
+        responseUser.firstName,
+        responseUser.lastName
+      );
+    })
+    .catch((error) => {
+      console.log('Signup system offline');
+    });
+}
