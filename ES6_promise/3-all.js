739752609331473859759913b7c3ccd6000e0391
Jhony <Jhonny14_1@hotmail.com>
2024-04-13 10:50:37 -0500

    chore(3-all): Se corrige salida.

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,11 +5,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([responsePhoto, responseUser]) => {
-      console.log(
-        responsePhoto.body,
-        responseUser.firstName,
-        responseUser.lastName,
-      );
+      console.log(`${responsePhoto.body} ${responseUser.firstName} ${responseUser.lastName}`);
     })
     .catch(() => new Error('Signup system offline'));
 }
