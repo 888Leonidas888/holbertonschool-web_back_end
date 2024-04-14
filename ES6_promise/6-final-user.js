81fb9ab09b947467b2839b7c14d5bd80d5a81224
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 17:55:49 -0500

    chore(6-final-user): Se corrige salida.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,15 @@
 // Handle multiple promises
+// import signUser from './4-user-promise';
+// import uploadPhoto from './5-photo-reject';
+
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled([
+//     signUser(firstName, lastName),
+//     uploadPhoto(fileName),
+//   ]).then((r) => r);
+// }
+
+// 6-final-user.js
 import signUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
@@ -6,5 +17,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((r) => r);
+  ]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      } else {
+        return {
+          status: result.status,
+          value: `Error: ${result.reason.message}`,
+        };
+      }
+    });
+  });
 }
