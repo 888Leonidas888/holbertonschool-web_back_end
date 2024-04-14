dc89046e7652633d7d0dd1bbbccca08482d4cd5d
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 18:58:03 -0500

    chore(8-try): Lanza un error (throw).

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,1 +1,17 @@
-console.log('Hi')
+// Throw an error
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
+
+//  Usando el operador ternaria
+// export default function divideFunction(numerator, denominator) {
+//   return denominator === 0
+//     ? (() => {
+//         throw new Error('cannot divide by 0');
+//       })()
+//     : numerator / denominator;
+// }
