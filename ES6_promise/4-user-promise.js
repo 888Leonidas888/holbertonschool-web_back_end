47e68ae3c0cf28ef95c4a83266351219d61021d8
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 15:56:30 -0500

    chore: Regresa un simple promesa resuelte.

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,1 +1,17 @@
-console.log('Hi')
+// Simple promise
+
+// Forma corta
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
+}
+
+// Forma larga
+// export default function signUpUser(firstName, lastName) {
+//   return new Promise((resolve) => {
+//     const data = {
+//       firstName,
+//       lastName,
+//     };
+//     resolve(data);
+//   });
+// }
