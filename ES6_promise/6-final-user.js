b7f2787c2ca14148faaf2de52a8e4f645d719b8f
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 17:04:14 -0500

    chore(6-final-user): Maneja multiples promesas y regresa un array(Promise.allSettled).

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,1 +1,10 @@
-console.log('Hi')
+// Handle multiple promises
+import signUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((r) => r);
+}
