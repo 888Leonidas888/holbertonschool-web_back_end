8cb5fd8fb48bc49baf187e3e2784a66272ea1f42
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 18:13:55 -0500

    chore(6-final-user): Se corrige estilo.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -9,7 +9,6 @@
 //   ]).then((r) => r);
 // }
 
-// 6-final-user.js
 import signUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
@@ -17,19 +16,9 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      } else {
-        return {
-          status: result.status,
-          value: `Error: ${result.reason.message}`,
-        };
-      }
-    });
-  });
+  ]).then((results) => results.map((result) => (
+    result.status === 'fulfilled'
+      ? { status: result.status, value: result.value }
+      : { status: result.status, value: `Error: ${result.reason.message}` }
+  )));
 }
