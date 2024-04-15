a7888e136095fe65ac9338459b7ce0c348095fb1
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 19:10:40 -0500

    chore(9-try): Captura los errores(try-catch-finally).

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,1 +1,14 @@
-console.log('Hi')
+// Throw error / try catch
+
+export default function guardrail(mathFunction) {
+  const MSG = 'Guardrail was processed';
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push(MSG);
+  }
+  return queue;
+}
