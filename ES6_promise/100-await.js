241ee6aaff146f4c60fea533678a4c0e3cd536f9
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 19:49:08 -0500

    chore(100-await): Función asíncrona (Await/Async).

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,1 +1,10 @@
-console.log('Hi')
+// Await / Async
+
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  return Promise.all([uploadPhoto(), createUser()]).then((results) => ({
+    photo: results[0],
+    user: results[1],
+  }));
+}
