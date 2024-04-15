1be9f8cf42509d86d887cb24714d2801da5d6225
Jhony <Jhonny14_1@hotmail.com>
2024-04-14 19:54:37 -0500

    chore(100-await): Se agegra catch

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -3,8 +3,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return Promise.all([uploadPhoto(), createUser()]).then((results) => ({
-    photo: results[0],
-    user: results[1],
-  }));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => ({
+      photo: results[0],
+      user: results[1],
+    }))
+    .catch(() => new Error('Error'));
 }
