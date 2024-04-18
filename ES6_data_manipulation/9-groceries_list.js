dc479c6812877512c51a2f23cfa93752b2c3f17b
Jhony <Jhonny14_1@hotmail.com>
2024-04-18 10:37:14 -0500

    feat(9-groceries_list): Regresa un map.

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+// Map data structure
+
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
