e86b13d4f66b79cb3ef91f5c065818b66cdac0a2
Jhony <Jhonny14_1@hotmail.com>
2024-04-18 10:47:40 -0500

    chore(8-clean_set): Se corrige salida, se agrega una validación para starStrings.

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
   const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-  return filteredValues
-    .map((value) => value.substring(startString.length))
-    .join('-');
+  if (startString !== '') {
+    return filteredValues
+      .map((value) => value.substring(startString.length))
+      .join('-');
+  }
+  return '';
 }
